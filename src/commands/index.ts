50d029d7f949d4ada4b35fad09423d7e3abf3b9a
Domas Klimavicius <dev@dklimavicius.com>
2024-03-21 20:13:59 +0200

    Add whitelist-player command

---
+++ src/commands/index.ts
@@ -0,0 +1,7 @@
+import * as ping from './ping'
+import * as whitelistadd from './whitelist-player'
+
+export const commands = {
+    ping,
+    whitelistadd
+}
---
+++ src/commands/whitelist-player.ts
@@ -0,0 +1,20 @@
+import { CommandInteraction, SlashCommandBuilder } from "discord.js"
+import {config} from "../config";
+
+const execSync = require('child_process').execSync
+
+export const data = new SlashCommandBuilder()
+    .setName('whitelistadd')
+    .setDescription('Whitelists a player')
+    .addStringOption(option => option.setName('username').setDescription('Minecraft username').setRequired(true))
+    .addStringOption(option => option.setName('ip').setDescription('Ip address').setRequired(true))
+
+export async function execute(interaction: CommandInteraction) {
+    const usernameIndex = interaction.options.data[0].name === 'username' ? 0 : 1
+    const ipIndex = usernameIndex === 0 ? 1 : 0
+
+    execSync(`tmux send-keys -t ${config.TMUX_SESSION_NAME} \"whitelist add ${interaction.options.data[usernameIndex]}\"`, { encoding: 'utf-8' })
+    execSync(`sudo iptables -A INPUT -p tcp -s ${interaction.options.data[ipIndex]} --dport 25565 -j ACCEPT`, { encoding: 'utf-8' })
+
+    return interaction.reply("Added")
+}
